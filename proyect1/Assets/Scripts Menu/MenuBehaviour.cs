96b248a328bbc5d7143fdb811264752a39adcc9e
AscencioPEPE <69373682+AscencioPEPE@users.noreply.github.com>
2023-04-03 12:17:55 -0600

    Added Scenes
    Creadas escenas de menu inicial

---
+++ proyect1/Assets/Scripts Menu/MenuBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void partidaNueva()
+    {
+        SceneManager.LoadScene("Partida Nueva Menu");
+    }
+    public void configuracionesScene()
+    {
+        SceneManager.LoadScene("Configuraciones");
+    }
+
+    public void salirButton()
+    {
+        Application.Quit();
+    }
+}
